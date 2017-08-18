3cdffbd2d10be61e8a1df529f6d86dd49d6fb498
robdel12 <robertdeluca19@gmail.com>
2017-08-18 17:54:37 -0500

    Throw error with no label passed, look for label in DOM
    This adds support for checkboxes nested in labels and for the ability
    to find a checkbox label if it's not passed in the constructor.

--- checkbox.js
+++ checkbox.js
@@ -3,14 +3,48 @@ class Checkbox {
     this.checkbox = checkboxEl;
     this.label = labelEl;
 
+    if (!this.checkbox) {
+      throw new Error(`You must pass a checkbox element`);
+    }
+
+    if (!this.label) {
+      this.label = this.searchForLabel();
+
+      if (this.label === false) {
+        throw new Error(`You must pass a label element for ${this.checkbox.id}`);
+      }
+    }
+
     this.init();
+  }
+
+  searchForLabel() {
+    if (this.checkbox.parentNode.tagName === 'LABEL') {
+      return this.checkbox.parentNode;
+    } else if(this.checkbox.id) {
+      // try to find a label by matching the for attr
+      return document.querySelector(`[for=${this.checkbox.id}`) || false;
+    }
+
+    return false;
   }
 
   init() {
     this.checkbox.onclick = this.toggleCheckbox.bind(this);
     this.label.onclick = this.labelClick.bind(this);
     this.checkbox.onkeypress = this.checkboxKeyPress.bind(this);
-    this.checkbox.tabindex = 0;
+    this.initA11y();
+  }
+
+  initA11y() {
+    this.checkbox.setAttribute('tabindex', 0);
+    this.checkbox.setAttribute('role', 'checkbox');
+    this.checkbox.setAttribute('aria-labelledby', this.label.id);
+    this.checkbox.setAttribute('aria-checked', 'false');
+  }
+
+  get isChecked() {
+    return this.checkbox.classList.contains('is-checked');
   }
 
   checkboxKeyPress(event) {
@@ -21,16 +55,18 @@ class Checkbox {
     }
   }
 
-  labelClick() {
-    this.toggleCheckbox();
+  labelClick(event) {
+    // If the click isn't on the label, don't do anything.
+    if (event.target !== this.label) {
+      return false;
+    }
+
+    this.toggleCheckbox(event);
     this.checkbox.focus();
   }
 
-  toggleCheckbox() {
-    let isCurrentlyChecked = this.checkbox.classList.contains('is-checked');
-    let ariaChecked = this.checkbox.getAttribute('aria-checked') === 'true';
-
-    this.checkbox.classList.toggle('is-checked', !isCurrentlyChecked);
-    this.checkbox.setAttribute('aria-checked', !ariaChecked);
+  toggleCheckbox(event) {
+    this.checkbox.classList.toggle('is-checked', !this.isChecked);
+    this.checkbox.setAttribute('aria-checked', this.isChecked);
   }
 }
